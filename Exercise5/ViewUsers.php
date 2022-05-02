0757f6018a9c5ae931317d3c12d2d02b51508b47
Jordan <jcedeno870@gmail.com>
2022-05-02 14:49:46 -0500

    Implemented ViewUsers

---
+++ Exercise5/ViewUsers.php
@@ -0,0 +1,26 @@
+<?php 
+$hostname = "129.237.87.5";
+$username = "j149c752";
+$password = "LekeiN7c";
+$database = "j149c752";
+$mysqli = new mysqli($hostname, $username, $password, $database);
+
+if ($mysqli->connect_errno) { 
+    printf("Connect failed: %s\n", $mysqli->connect_error); 
+    exit(); 
+}
+
+$query = "SELECT * FROM Users";
+$result = mysqli_query($mysqli, $query);
+
+echo "<table border =\"1\">";
+echo "<th>Users</th>";
+while($row = mysqli_fetch_array($result)){
+    echo "<tr>";
+    echo "<td> . $row[0] . </td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+$mysqli->close();
+?>
